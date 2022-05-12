9a82b13577a7e7a84d4c276704d6ea0f82989d39
loaymoolb <loya130513@gmail.com>
2022-05-12 22:44:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var menu = document.querySelector('.hamburger');
+var menu = document.querySelector('.burger-menu');
 menu.addEventListener('click', function(){
     document.querySelector('.nav').classList.toggle('toggle');
 })
